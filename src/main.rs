6a83a885ea54639d0a27a235b518d88c019c20df
libiao7 <31200418+libiao7@users.noreply.github.com>
2026-01-11 03:26:59 +0800

    抖音下载

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,8 @@ use std::sync::Arc;
 use tokio::sync::Semaphore;
 // use tokio::task::JoinSet;
 // use tokio_postgres::NoTls;
+// use tokio::fs::File;
+use tokio::io::AsyncWriteExt;
 
 #[derive(Debug, serde::Serialize)]
 struct Item {
@@ -38,6 +40,172 @@ struct AppState {
     download_semaphore: Arc<Semaphore>, // 全局下载信号量
 }
 
+#[derive(serde::Serialize, serde::Deserialize, Debug)]
+struct DouYinDownloadTask {
+    url: String,
+    file_name: String,
+}
+
+#[derive(serde::Serialize, serde::Deserialize, Debug)]
+struct DouYinDownloadReq {
+    sec_uid: String, //网址 用于文件夹      MS4wLjABAAAA6Ks9K7OGdw7IlxnL1OlAAaGWnh9QIzmaPqQm985hNxU
+    douyin_download_tasks: Vec<DouYinDownloadTask>,
+    nickname: String,  //常变化的.昵称  用于aweme_json文件夹命名
+    user_json: String, //常变化的.昵称  用于aweme_json文件夹命名
+    aweme_avatar_url: String, //常变化的.昵称  用于aweme_json文件夹命名
+                       // author_user_id: u64,    //唯一id
+                       // douyin_account: String, //180天可以改一次,抖音号
+                       // douyin_user_page_html: String,
+}
+
+async fn download_douyin_user_awemes(
+    axum::extract::State(state): axum::extract::State<Arc<AppState>>,
+    axum::extract::Json(douyin_download_req): axum::extract::Json<DouYinDownloadReq>,
+) -> impl axum::response::IntoResponse {
+    let sec_uid = &douyin_download_req.sec_uid;
+    let nickname = &douyin_download_req.nickname;
+    let user_json = &douyin_download_req.user_json;
+    let aweme_avatar_url = &douyin_download_req.aweme_avatar_url;
+    let total_count = douyin_download_req.douyin_download_tasks.len();
+    let dir_path = &std::path::Path::new("C:\\Users\\aa\\d-y").join(&sec_uid);
+
+    if !dir_path.exists() {
+        tokio::fs::create_dir_all(dir_path)
+            .await
+            .expect("Failed to create directory");
+    }
+
+    let mut joinset = tokio::task::JoinSet::new();
+
+    for douyin_download_task in douyin_download_req.douyin_download_tasks {
+        let file_path = dir_path
+            .join(sanitize_windows_filename_strict(&douyin_download_task.file_name).unwrap());
+        let url = douyin_download_task.url;
+        let semaphore = state.download_semaphore.clone(); // 使用全局信号量
+        let task_client = state.http_client.clone();
+        joinset.spawn(async move {
+            let _permit = semaphore.acquire_owned().await.unwrap();
+
+            if file_path.exists() {
+                return Ok("existed");
+            }
+
+            match download_image(&task_client, &url, &file_path, false).await {
+                Ok(_) => Ok("OK"),
+                Err(e) => Err((format!("e: {}: {}", e, url), url)),
+            }
+        });
+    }
+
+    let mut success_count: u32 = 0;
+    let mut failed_urls = Vec::new();
+
+    while let Some(res) = joinset.join_next().await {
+        match res {
+            Ok(Ok(t)) => {
+                success_count += 1;
+                println!("{t}: {total_count}---{success_count}");
+            }
+            Ok(Err((err_info, url))) => {
+                eprintln!("err_info: {err_info} : {url}");
+                failed_urls.push(url);
+            }
+            Err(e) => eprintln!("Task panicked: {:?}", e),
+        }
+    }
+    println!("{nickname} : 作品下载已完成！");
+    if failed_urls.is_empty() {
+        let failed_file_path = dir_path.join("failed_downloads.html");
+        if failed_file_path.exists() {
+            tokio::fs::remove_file(failed_file_path)
+                .await
+                .expect("Failed to delete failed_downloads.html");
+        }
+        let user_info_dir_path = &dir_path.join("user-info");
+        tokio::fs::create_dir_all(user_info_dir_path)
+            .await
+            .expect("Failed to create directory");
+        tokio::fs::write(user_info_dir_path.join("user.json"), user_json)
+            .await
+            .unwrap();
+        tokio::fs::write(
+            user_info_dir_path.join(format!(
+                "{}@{}.json",
+                std::time::SystemTime::now()
+                    .duration_since(std::time::UNIX_EPOCH)
+                    .unwrap()
+                    .as_millis(),
+                sanitize_windows_filename_strict(nickname).unwrap()
+            )),
+            user_json,
+        )
+        .await
+        .unwrap();
+
+        // 只请求一次
+        match state.http_client.get(aweme_avatar_url).send().await {
+            Ok(resp) if resp.status().is_success() => {
+                if let Ok(bites) = resp.bytes().await {
+                    let path1 = user_info_dir_path.join(format!(
+                        "{}@{}.jpeg",
+                        std::time::SystemTime::now()
+                            .duration_since(std::time::UNIX_EPOCH)
+                            .unwrap()
+                            .as_millis(),
+                        sanitize_windows_filename_strict(nickname).unwrap()
+                    ));
+                    let path2 = user_info_dir_path.join("avatar.jpeg");
+
+                    tokio::fs::write(path1, &bites).await.unwrap();
+                    tokio::fs::write(path2, &bites).await.unwrap();
+                    println!("{nickname} : 头像 完成");
+                }
+            }
+            _ => eprintln!("头像及副本下载失败"),
+        }
+    } else {
+        let html_content = format!(
+            r#"<html><body><h1><a href="https://www.douyin.com/user/{}">{}</a></h1><ul>{}</ul></body></html>"#,
+            sec_uid,
+            nickname,
+            failed_urls
+                .iter()
+                .map(|url| format!("<li><a href=\"{}\">{}</a></li>", url, url))
+                .collect::<Vec<_>>()
+                .join("")
+        );
+        tokio::fs::write(dir_path.join("failed_downloads.html"), html_content)
+            .await
+            .expect("Failed to write HTML file");
+    }
+
+    std::process::Command::new("C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe")
+        .arg(dir_path.to_str().unwrap())
+        .spawn()
+        .unwrap();
+
+    format!("{nickname} : 已完成！")
+}
+
+fn sanitize_windows_filename_strict(filename: &str) -> Result<String, &str> {
+    // 1. 替换非法字符
+    let sanitized: String = filename
+        .chars()
+        .map(|c| match c {
+            '<' | '>' | ':' | '"' | '/' | '\\' | '|' | '?' | '*' | '\u{0000}'..='\u{001F}' => '-',
+            _ => c,
+        })
+        .collect();
+
+    // 2. 去除末尾的空格和点
+    let trimmed = sanitized.trim_end_matches(|c| c == ' ' || c == '.');
+
+    // 3. 如果结果为空（例如输入全是合法但被 trim 掉的字符），提供一个默认值
+    if trimmed.is_empty() {
+        return Err("empty now");
+    }
+    Ok(trimmed.to_string())
+}
 async fn get_items_batch_pq(
     axum::extract::State(state): axum::extract::State<Arc<AppState>>,
     axum::extract::Json(search_request): axum::extract::Json<SearchRequest>,
@@ -80,10 +248,12 @@ async fn handle_post(
     axum::extract::Json(data): axum::extract::Json<ImageData>,
 ) -> impl axum::response::IntoResponse {
     let page_url = &data.page_url;
-    let dir_path = std::path::Path::new("C:\\Users\\aa\\Desktop\\zup").join(&data.title);
+    let dir_path = &std::path::Path::new("C:\\Users\\aa\\Desktop\\zup").join(&data.title);
 
     if !dir_path.exists() {
-        std::fs::create_dir_all(&dir_path).expect("Failed to create directory");
+        tokio::fs::create_dir_all(dir_path)
+            .await
+            .expect("Failed to create directory");
     }
 
     let mut joinset = tokio::task::JoinSet::new();
@@ -101,7 +271,7 @@ async fn handle_post(
                 return Ok("existed");
             }
 
-            match download_image(task_client, &url, &file_path).await {
+            match download_image(&task_client, &url, &file_path, false).await {
                 Ok(_) => Ok("OK"),
                 Err(e) => {
                     eprintln!("e: {}: {}", e, url);
@@ -125,7 +295,7 @@ async fn handle_post(
             Err(e) => eprintln!("Task panicked: {:?}", e),
         }
     }
-    println!("{}\n已完成！", &data.title);
+    println!("{} : 已完成！", &data.title);
 
     if !failed_urls.is_empty() {
         let html_content = format!(
@@ -138,29 +308,40 @@ async fn handle_post(
                 .collect::<Vec<_>>()
                 .join("")
         );
-        std::fs::write(dir_path.join("failed_downloads.html"), html_content)
+        tokio::fs::write(dir_path.join("failed_downloads.html"), html_content)
+            .await
             .expect("Failed to write HTML file");
     } else {
         let failed_file_path = dir_path.join("failed_downloads.html");
         if failed_file_path.exists() {
-            std::fs::remove_file(failed_file_path).expect("Failed to delete failed_downloads.html");
+            tokio::fs::remove_file(failed_file_path)
+                .await
+                .expect("Failed to delete failed_downloads.html");
         }
     }
 
-    let _ =
-        std::process::Command::new("C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe")
-            .arg(dir_path.to_str().unwrap())
-            .output();
+    std::process::Command::new("C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe")
+        .arg(dir_path.to_str().unwrap())
+        .spawn()
+        .unwrap();
 
-    format!("{}\n已完成！", &data.title)
+    format!("{} : 已完成！", &data.title)
 }
 
 async fn download_image(
-    client: reqwest::Client,
+    client: &reqwest::Client,
     url: &str,
     path: &std::path::Path,
+    re_write: bool,
 ) -> Result<(), String> {
-    let response = client.get(url).send().await.map_err(|e| e.to_string())?;
+    let mut request_builder = client.get(url);
+    if let Ok(parsed_url) = reqwest::Url::parse(url) {
+        if let Some(host) = parsed_url.host_str() {
+            request_builder =
+                request_builder.header("Referer", format!("{}://{}/", parsed_url.scheme(), host));
+        }
+    }
+    let response = request_builder.send().await.map_err(|e| e.to_string())?;
 
     if !response.status().is_success() {
         return Err(format!("Failed to download image: {}", response.status()));
@@ -170,10 +351,18 @@ async fn download_image(
     if content.is_empty() {
         return Err("Downloaded file is empty".to_string());
     }
-
-    let mut file = std::fs::File::create_new(path).map_err(|e| e.to_string())?;
-    std::io::copy(&mut content.as_ref(), &mut file).map_err(|e| e.to_string())?;
-    // std::fs::write(&path, content.as_ref()).map_err(|e| e.to_string())?;
+    if re_write {
+        tokio::fs::write(path, content)
+            .await
+            .map_err(|e| e.to_string())?;
+    } else {
+        tokio::fs::File::create_new(path)
+            .await
+            .map_err(|e| e.to_string())?
+            .write_all(&content)
+            .await
+            .map_err(|e| e.to_string())?;
+    }
 
     Ok(())
 }
@@ -206,6 +395,7 @@ async fn main() {
     });
     let app = axum::Router::new()
         .route("/zup", axum::routing::post(handle_post))
+        .route("/download_douyin_user_awemes", axum::routing::post(download_douyin_user_awemes))
         .route("/rarbg/batch_pq", axum::routing::post(get_items_batch_pq))
         .route(
             "/download_poster/{override}/{product_number}/{video_poster}",
@@ -226,9 +416,9 @@ async fn main() {
                                     .media_type()
                                     .to_string(),
                             )],
-                            bytes::Bytes::from(std::fs::read(&cover_path).unwrap()),
+                            bytes::Bytes::from(tokio::fs::read(&cover_path).await.unwrap()),
                         ),
-                        (Ok(exists),_) => {
+                        (Ok(existing),_) => {
                             let resp = reqwest::get(&poster_url).await.unwrap();
                             let web_content_type = resp.headers().get(reqwest::header::CONTENT_TYPE).expect(&poster_url)
                                 .to_str()
@@ -239,15 +429,8 @@ async fn main() {
                                 .unwrap()
                                 .to_string();
                             let bites = resp.bytes().await.unwrap();
-                            if web_content_type.to_lowercase().starts_with("image/") && !(2733 > web_content_length.parse().unwrap() && poster_url.starts_with("https://awsimgsrc.dmm.co.jp/pics_dig/digital/video/"))&&!(exists&&(&poster_url).starts_with("https://awsimgsrc.dmm.co.jp/pics_dig/digital/video/")&&std::fs::read(&cover_path).unwrap().len()==web_content_length.parse::<usize>().unwrap()) {
-                                // std::io::copy(
-                                //     &mut bites.as_ref(),
-                                //     &mut std::fs::File::create(&cover_path).unwrap(),
-                                // )
-                                // .unwrap();
-
-                                // std::fs::File::create(&cover_path).unwrap().write_all(bites.as_ref()).unwrap();
-                                std::fs::write(&cover_path,bites.as_ref()).unwrap();
+                            if web_content_type.to_lowercase().starts_with("image/") && !(2733 > web_content_length.parse().unwrap() && poster_url.starts_with("https://awsimgsrc.dmm.co.jp/pics_dig/digital/video/"))&&!(existing&&(&poster_url).starts_with("https://awsimgsrc.dmm.co.jp/pics_dig/digital/video/")&&tokio::fs::read(&cover_path).await.unwrap().len()==web_content_length.parse::<usize>().unwrap()) {
+                                tokio::fs::write(&cover_path,&bites).await.unwrap();
                             }
                             (
                                 [(axum::http::header::CONTENT_TYPE, web_content_type)],
